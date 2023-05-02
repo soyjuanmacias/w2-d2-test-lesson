4f0229d657de9774bd421257988f23aacc8b3cba
Juan Macías <info@juanmacias.es>
2023-05-02 15:42:58 +0200

    Add explication part

---
+++ README.md
@@ -0,0 +1,4 @@
+# 6 Tipos de expresiones
+## Aritméticas, Asígnación, comparación, lógicas, función, objecto
+
+# Expresión de ¿Llamada?
---
+++ index.js
@@ -0,0 +1,67 @@
+// ¿A qué resuelve una expresión de llamada?
+// Una expresión de llamada resuelve al valor que devuelve 
+// resolver, solucionar, arreglar, fix, bugfix
+
+// Expresión tipo (id)entificador = Variable que señala, señores que apuntan con el dedo 🫵🏻
+// Expresión --> expresar, decir, contar hablar, exponer, señalar
+
+// Expresión literal - Literatura, libros -> Expresar lectura
+// {}, () => {}, 
+
+// Silbido fijo = Asignación
+// Silbido intermitente = Leer+
+// Plop = Crear nuevo valor, función u objeto.
+
+/**
+ * Ej. Pizarra
+ * Creamos variable [_____]. Es una variable de (id)entificador
+ * Respirar --> breathe, bread, --> pan, parar, respirar
+ * Miramos a la derecha para: Resolver, bifurcación, creamos un {} o () => {}  señalamos a primitivo
+ *      * Plop + Silbido Largo + Creamos un {} o () => {} o señalamos primitivo --> Silbido largo
+ *      * Silbido intermitente en caso de lectura
+ * 
+ */
+
+/**
+ * Funciones (repetir pregunta)
+ * 
+ * Pregunta
+ * params, code, return
+ * qué recibe, que hace, qué retorna
+ */
+
+/**
+ * Intellisense IMPORTANTE
+ */
+
+
+/**
+ * Testing
+ * 1. Concepto BDD
+ * 2. jsdoc
+ * 3. comparten reglas de negocio y comportamiento todos los equipos
+ * 
+ * Me compro una máquina de estampar camisetas. Tienes 15 días para devolverla. Si no funciona bien me la devuelves.
+ * Pen drive de unicornio con la foto de gozilla y camiseta lila.
+ * 
+ * Explicación de los Casos de uso con los posibles usos de la máquina.
+ * 
+ * Corner case: Casos de usos que no se suelen dar o repetir
+ * 
+ */
+
+/**
+ * Refactorizar es alterar un código sin modificar la funcionalidad
+ */
+
+/**
+ * Tests
+ * - Explicación de Jest y naming archivos.
+ * - Explicación @types/jest --> 
+ * - Explicación tag DT npm
+ * - Errores (Error de configuración)
+ * - Instalación transpiler babel
+ * - AAA
+ * - 
+ * 
+ */
